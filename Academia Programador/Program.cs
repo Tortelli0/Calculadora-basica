8c8406200620625353f0208d16eb308469bcf54a
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-03-13 17:34:53 -0300

    2 caminhos diferentes

--- Academia Programador/Program.cs
+++ Academia Programador/Program.cs
@@ -1,42 +1,111 @@
 ﻿#nullable disable
+using System.Net;
+
 namespace AP;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Deseja utilizar a calculadora? (s/n)");
+        Console.WriteLine("Bem vindo a Calculadora!");
+        Console.WriteLine("Deseja começar uma operação? (s/n)");
         string calculo = Console.ReadLine();
 
-        while (calculo == "s")
+        Console.WriteLine("\nDigite 1 para realizar todas as operações e 2 para escolher a operação: ");
+        int opçoes = int.Parse(Console.ReadLine());
+
+        if (opçoes == 1)
         {
-            if (calculo == "n")
+            while (calculo == "s")
             {
+                if (calculo == "n")
+                {
+                    break;
+                }
+
+                Console.WriteLine("\nDigite o primeiro número: ");
+                double n1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nDigite o segundo número: ");
+                double n2 = double.Parse(Console.ReadLine());
+
+                while (n2 == 0)
+                {
+                    Console.WriteLine("\nO segundo número não poder ser zero, digite novamente.");
+
+                    Console.WriteLine("\nDigite o segundo número novamente: ");
+
+                    n2 = double.Parse(Console.ReadLine());
+                    break;
+                }
+
+                double resultadosoma = n1 + n2;
+                double resultadomultiplicacao = n1 * n2;
+                double resultadodivisao = n1 / n2;
+                double resultadosubtracao = n1 - n2;
+
+                Console.WriteLine($"\nO resultado da soma destes dois números é: {resultadosoma}");
+                Console.WriteLine($"\nO resultado da multiplicação destes dois números é: {resultadomultiplicacao}");
+                Console.WriteLine($"\nO resultado da divisão destes dois números é: {resultadodivisao}");
+                Console.WriteLine($"\nO resultado da subtração destes dois números é: {resultadosubtracao}");
+                Console.WriteLine("\nDeseja continuar utilizando a calculadora? (s/n)");
+                calculo = Console.ReadLine();
                 break;
+
             }
-
-            Console.WriteLine("Digite o primeiro número: ");
-            double n1 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo número: ");
-            double n2 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o terceiro número: ");
-            double n3 = double.Parse(Console.ReadLine());
-
-            double resultadosoma = n1 + n2 + n3;
-            double resultadomultiplicacao = n1 * n2 * n3;
-            double resultadodivisao = (n1 / n2) / n3;
-            double resultadosubtracao = (n1 - n2) - n3;
-
-            Console.WriteLine($"O resultado da soma destes dois números é: {resultadosoma}");
-            Console.WriteLine($"O resultado da multiplicação destes dois números é: {resultadomultiplicacao}");
-            Console.WriteLine($"O resultado da divisão destes dois números é: {resultadodivisao}");
-            Console.WriteLine($"O resultado da subtração destes dois números é: {resultadosubtracao}");
-            
-            Console.WriteLine("Deseja continuar utilizando a calculadora? (s/n)");
-            calculo = Console.ReadLine();
-
         }
+
+            if  (opçoes == 2 )
+            {
+                while (calculo == "s")
+                {
+                    if (calculo == "n")
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("\nDigite 1 para Adicionar");
+                    Console.WriteLine("Digite 2 para Subtrair");
+                    Console.WriteLine("Digite 3 para Multiplicar");
+                    Console.WriteLine("Digite 4 para Dividir");
+                    Console.WriteLine("Digite S para sair\n");
+                    double pergunta = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite o primeiro número: ");
+                    int n3 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite o segundo número: ");
+                    int n4 = int.Parse(Console.ReadLine());
+
+                    int resultado = 0;
+
+                    if (pergunta == 1)
+                    {
+                        resultado = n3 + n4;
+                    }
+
+                    else if (pergunta == 2)
+                    {
+                        resultado = n3 - n4;
+                    }
+
+                    else if (pergunta == 3)
+                    {
+                        resultado = n3 * n4;
+                    }
+
+                    else if (pergunta == 4)
+                    {
+                        resultado = n3 / n4;
+                    }
+
+                    else if (pergunta == 5)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine($"\nO resultado da sua operação é: {resultado}");
+                }
+            }
     }
 }
